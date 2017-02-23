03ba5b34c027cbb39a0a78ae378d5e94b84d4de7
Yoshinori Hirose <44sheetsofseaweed@gmail.com>
2017-02-23 21:56:40 +0900

    fix --- Handle Empty or Null Cases

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -58,9 +58,17 @@ public class MainActivity extends AppCompatActivity {
 
     private class Task extends AsyncTask<String, Void, Event> {
         protected Event doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
             return Utils.fetchEarthquakeData(urls[0]);
         }
         protected void onPostExecute(Event result) {
+            // If there is no result, do nothing.
+            if (result == null) {
+                return;
+            }
             // Update the information displayed to the user.
             updateUi(result);
         }
